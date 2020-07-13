2ff9a8a120420b9d31a2a30137e1a3f2bda48dce
dmessina <danilo.messina@everis.com>
2020-07-13 12:59:52 +0200

    adding FIELD in annotation Target

--- api/rest/src/main/java/it/gov/pagopa/bpd/common/annotation/UpperCase.java
+++ api/rest/src/main/java/it/gov/pagopa/bpd/common/annotation/UpperCase.java
@@ -3,7 +3,7 @@ package it.gov.pagopa.bpd.common.annotation;
 
 import java.lang.annotation.*;
 
-@Target(ElementType.PARAMETER)
+@Target({ElementType.PARAMETER,ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface UpperCase {
