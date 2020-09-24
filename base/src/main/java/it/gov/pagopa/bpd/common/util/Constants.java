18589b5d02beb5078bc02fba36c1fc995d802d24
dmessina <danilo.messina@everis.com>
2020-09-24 15:23:39 +0200

    checkIban implementation

--- base/src/main/java/it/gov/pagopa/bpd/common/util/Constants.java
+++ base/src/main/java/it/gov/pagopa/bpd/common/util/Constants.java
@@ -4,4 +4,5 @@ public final class Constants {
 
     public static final long FUTURE_OR_PRESENT_TOLERANCE = 60;
     public static final String FISCAL_CODE_REGEX = ".*^([a-zA-Z]{6}[0-9]{2}[a-zA-Z][0-9]{2}[a-zA-Z][0-9]{3}[a-zA-Z]).*";
+    public static final String IBAN_REGEX = ".*^([iI][tT][0-9]{2}[a-zA-Z][0-9]{22}).*";
 }
