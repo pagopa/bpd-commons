69dbacaae87991e9430e201d1a317a07dc005395
alecastagno <alessandro.castagno.sia@gmail.com>
2020-12-09 17:42:35 +0100

    fix fiscalCode regular expression

--- base/src/main/java/it/gov/pagopa/bpd/common/util/Constants.java
+++ base/src/main/java/it/gov/pagopa/bpd/common/util/Constants.java
@@ -3,6 +3,6 @@ package it.gov.pagopa.bpd.common.util;
 public final class Constants {
 
     public static final long FUTURE_OR_PRESENT_TOLERANCE = 60;
-    public static final String FISCAL_CODE_REGEX = "^([A-Za-z]{6}[0-9lmnpqrstuvLMNPQRSTUV]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9lmnpqrstuvLMNPQRSTUV]{2}[A-Za-z]{1}[0-9lmnpqrstuvLMNPQRSTUV]{3}[A-Za-z]{1})|([0-9]{11})$";
+    public static final String FISCAL_CODE_REGEX = "^([A-Za-z]{6}[0-9lmnpqrstuvLMNPQRSTUV]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9lmnpqrstuvLMNPQRSTUV]{2}[A-Za-z]{1}[0-9lmnpqrstuvLMNPQRSTUV]{3}[A-Za-z]{1})$";
     public static final String IBAN_REGEX = ".*^([iI][tT][0-9]{2}[a-zA-Z][0-9]{22}).*";
 }
